c3643dd9c473e6a36570c3ecdae7dbf01dcec3ce
Karthik <theankitsingh2002@gmail.com>
2026-08-19 00:37:36 +0530

    commited

---
+++ src/data/jobs.js
@@ -0,0 +1,91 @@
+const jobs = [
+    {
+        id: 1,
+        title: "Frontend Developer",
+        company: "Google",
+        location: "Bangalore",
+        type: "Full Time",
+        mode: "Remote",
+        salary: "₹8 - ₹12 LPA",
+        category: "Development"
+    },
+
+    {
+        id: 2,
+        title: "React Developer Intern",
+        company: "Microsoft",
+        location: "Hyderabad",
+        type: "Internship",
+        mode: "Hybrid",
+        salary: "₹25K / Month",
+        category: "Development"
+    },
+
+    {
+        id: 3,
+        title: "UI/UX Designer",
+        company: "Adobe",
+        location: "Noida",
+        type: "Full Time",
+        mode: "Remote",
+        salary: "₹6 - ₹10 LPA",
+        category: "Design"
+    },
+
+    {
+        id: 4,
+        title: "Data Analyst Intern",
+        company: "Amazon",
+        location: "Bangalore",
+        type: "Internship",
+        mode: "On-site",
+        salary: "₹30K / Month",
+        category: "Data Science"
+    },
+
+    {
+        id: 5,
+        title: "Backend Developer",
+        company: "Infosys",
+        location: "Pune",
+        type: "Full Time",
+        mode: "Hybrid",
+        salary: "₹7 - ₹11 LPA",
+        category: "Development"
+    },
+
+    {
+        id: 6,
+        title: "Marketing Intern",
+        company: "Swiggy",
+        location: "Mumbai",
+        type: "Internship",
+        mode: "On-site",
+        salary: "₹20K / Month",
+        category: "Marketing"
+    },
+
+    {
+        id: 7,
+        title: "Logical bulider",
+        company: "Google",
+        location: "Banglore",
+        type: "Full Time",
+        mode: "Remote",
+        salary: "₹20L / Month",
+        category: "Data Structure"
+    },
+
+    {
+        id: 8,
+        title: "AI/ML",
+        company: "Clance",
+        location: "Lucknow",
+        type: "Full Time",
+        mode: "On-site",
+        salary: "₹80K / Month",
+        category: "Artificial Intelligence"
+    }
+];
+
+export default jobs;
