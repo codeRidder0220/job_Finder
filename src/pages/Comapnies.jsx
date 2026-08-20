3888e3bd0141a969d1d48b2e921716dc9e991fab
Karthik <theankitsingh2002@gmail.com>
2026-08-20 15:52:54 +0530

    img add

--- src/pages/Comapnies.jsx
+++ src/pages/Comapnies.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import build from '../assets/building.png'
 
 const Comapnies = () => {
   return (
     <div>
 
-      <div className='max-w-7xl mx-auto mt-8'>
+      <div className='flex gap-5 items-center max-w-7xl mx-auto mt-8'>
+        
+        <img src={build} alt="" className='w-50'/>
+        <div>
         <h1 className='text-8xl font-bold text-[#334155]'>Top Companies...</h1>
-        <p className='text-3xl pt-6 font-medium text-[#D97706] '>Explore companies hiring for jobs and internships.</p>
+        <p className=' text-3xl pt-6 pl-5 font-medium text-[#D97706] '>Explore companies hiring for jobs and internships.</p>
+        </div>
+       
       </div>
+
+
+
+      
       
     </div>
   )
