e1579a9391b7c2e9021aa26efe9de9677eb353fb
Karthik <theankitsingh2002@gmail.com>
2026-08-20 15:42:25 +0530

    change

---
+++ src/data/companies.js
@@ -0,0 +1,51 @@
+const companies = [
+    {
+        id: 1,
+        name: "Google",
+        location: "Bangalore",
+        openings: 24,
+        category: "Technology"
+    },
+
+    {
+        id: 2,
+        name: "Microsoft",
+        location: "Hyderabad",
+        openings: 18,
+        category: "Technology"
+    },
+
+    {
+        id: 3,
+        name: "Amazon",
+        location: "Bangalore",
+        openings: 31,
+        category: "E-Commerce"
+    },
+
+    {
+        id: 4,
+        name: "Infosys",
+        location: "Pune",
+        openings: 42,
+        category: "IT Services"
+    },
+
+    {
+        id: 5,
+        name: "clance",
+        location: "Noida",
+        openings: 12,
+        category: "IT Services"
+    },
+
+    {
+        id: 6,
+        name: "swiggy",
+        location: "Delhi",
+        openings: 80,
+        category: "IT Services"
+    }
+];
+
+export default companies;
--- src/pages/Comapnies.jsx
+++ src/pages/Comapnies.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 const Comapnies = () => {
   return (
     <div>
+
+      <div className='max-w-7xl mx-auto mt-8'>
+        <h1 className='text-8xl font-bold text-[#334155]'>Top Companies...</h1>
+        <p className='text-3xl pt-6 font-medium text-[#D97706] '>Explore companies hiring for jobs and internships.</p>
+      </div>
       
     </div>
   )
